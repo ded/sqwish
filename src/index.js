a903170a60dc2c24be9b18e0c40ad741517aa1de
Dustin Diaz <polvero@gmail.com>
2011-04-25 14:08:40 -0700

    update selector,dec,prop,val regex

--- src/index.js
+++ src/index.js
@@ -25,7 +25,6 @@ function sqwish(css, strict) {
   // allow /*! bla */ style comments to retain copyrights etc.
   var comments = css.match(/\/\*![\s\S]+?\*\//g);
 
-
   css = css.trim() // give it a solid trimming to start
 
     // comments
@@ -34,15 +33,12 @@ function sqwish(css, strict) {
     // line breaks and carriage returns
     .replace(/[\n\r]/g, '')
 
-    // space between selectors and declarations
-    .replace(/([^\{]*)\s+([{}])\s+([\w\*\#\.\-])/g, '$1$2$3')
-
-    // space between declarations, properties and values
-    .replace(/([:;,])\s+/g, '$1')
+    // space between selectors, declarations, properties and values
+    .replace(/\s*([:;,{}])\s*/g, '$1')
 
     // space between last declaration and end of rule
     // also remove trailing semi-colons on last declaration
-    .replace(/[\s;]+(})/g, '$1')
+    .replace(/;}/g, '}')
 
     // this is important
     .replace(/\s+(!important)/g, '$1')
